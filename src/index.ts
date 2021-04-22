8c3f883780465c9777b4502f1bb5274dfdd6798d
Stanislav Mykhailenko <stas.m@2muchcoffee.com>
2021-04-22 13:46:21 +0300

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import express, { Application } from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+
+const app: Application = express();
+
+const MONGO_URI = process.env.MONGO_URI;
+if (MONGO_URI) {
+  mongoose.connect(MONGO_URI, {
+    useFindAndModify: true,
+    useNewUrlParser: true,
+  });
+  const db = mongoose.connection;
+  db.on('error', (error) => console.log(error));
+  db.once('open', () => console.log('connected to db'));
+}
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on the port ${PORT}`);
+});
